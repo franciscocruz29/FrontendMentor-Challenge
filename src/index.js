02bddd0a477117340f66f972cb3e24b0b223771b
franciscocruz29 <franciscohuertas25@gmail.com>
2023-01-04 11:06:16 -0500

    Added javascript files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap-grid.css'
+import 'bootstrap/dist/css/bootstrap-utilities.css'
+import './styles/index.css'
+
+import app from './js/app'
+addEventListener('DOMContentLoaded', app())
---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+import handleScrollButton from './handleScrollButton'
+import animation from './animation'
+import handleMessageForm from './handleMessageForm'
+
+const app = () => {
+   // Intro Section
+   setTimeout(() => {
+      document.body.querySelector('.intro-section').remove()
+      document.body.style.overflow = 'auto'
+   }, 5250)
+
+   // Execute scroll button
+   handleScrollButton()
+
+   // Execute Animation
+   animation()
+
+   // Execute handle message form
+   handleMessageForm()
+}
+
+export default app
